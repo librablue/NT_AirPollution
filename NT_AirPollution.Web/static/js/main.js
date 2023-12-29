86aa475bee7752059d579c7456a9aa3c86dcf564
boss <librablue1985@gmail.com>
2023-12-29 16:43:00 +0800

    main.js

--- NT_AirPollution.Web/static/js/main.js
+++ NT_AirPollution.Web/static/js/main.js
@@ -1,21 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
 	// 數字欄位不能打[-+e]
-	const numberInput = document.querySelectorAll('input[type="number"]');
-	numberInput.forEach(item => {
-		item.addEventListener('keydown wheel paste', function (e) {
+	document.addEventListener('keydown', function (e) {
+		if (e.target.tagName === 'INPUT' && e.target.type === 'number') {
 			if (e.type === 'wheel') {
-				$(this).blur();
+				e.target.blur();
 				return;
 			}
+
 			var invalidChars = ['-', '+', 'e', 'ArrowUp', 'ArrowDown'];
+
 			// 如果輸入非允許字元停止
 			if (invalidChars.indexOf(e.key) > -1) {
 				e.preventDefault();
 			}
+
 			// 如果已經是負數且輸入負號，清空內容
-			if (+e.target.value < 0 && e.key === '-') {
+			if (parseFloat(e.target.value) < 0 && e.key === '-') {
 				e.target.value = '';
 			}
-		});
+		}
+	});
+
+	document.addEventListener('paste', function (e) {
+		if (e.target.tagName === 'INPUT' && e.target.type === 'number') {
+			e.preventDefault();
+		}
 	});
 });
