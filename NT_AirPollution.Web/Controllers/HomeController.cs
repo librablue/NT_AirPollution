ccee346e736143eb475840928671fc8379e5a48e
Hsieh <librablue1985@gmail.com>
2024-05-16 23:49:47 +0800

    回報調整

--- NT_AirPollution.Web/Controllers/HomeController.cs
+++ NT_AirPollution.Web/Controllers/HomeController.cs
@@ -10,10 +10,12 @@ namespace NT_AirPollution.Web.Controllers
     public class HomeController : Controller
     {
         private readonly NewsService _newsService =  new NewsService();
+        private readonly DownloadService _downloadService = new DownloadService();
 
         public ActionResult Index()
         {
             ViewBag.News = _newsService.GetNews().Take(5).ToList();
+            ViewBag.Downloads = _downloadService.GetDownloads().Take(5).ToList();
             return View();
         }
     }
