a8cc5ee90555be55f75e8b479baf400b225a05f7
boss <librablue1985@gmail.com>
2026-01-30 15:30:08 +0800

    結算繳費期限改成預設結算日+30天，超過30天則限當天

--- NT_AirPollution.Service/FormService.cs
+++ NT_AirPollution.Service/FormService.cs
@@ -1596,12 +1596,17 @@ namespace NT_AirPollution.Service
 
                 // 計算繳費資訊
                 var res = CalcPayment(info);
-                // 結算沒有滯納金&利息，繳費期限30天
+                // 結算沒有滯納金&利息，繳費期限為結算日+30天
                 if (!string.IsNullOrEmpty(form.AP_DATE1))
                 {
                     res.Interest = 0;
                     res.Penalty = 0;
-                    res.PayEndDate = DateTime.Now.AddDays(30);
+                    res.PayEndDate = info.ApplyDate.AddDays(30);
+                    // 如果超過30天就以今天為繳費期限
+                    if (DateTime.Now.Date > res.PayEndDate.Date)
+                    {
+                        res.PayEndDate = DateTime.Now;
+                    }
                 }
 
                 double sumPrice = Math.Round(res.CurrentPrice + res.Interest + res.Penalty, 0);
